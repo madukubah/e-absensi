c92ad2d3e9a24262b650fd482bc41091f2e44104
piyo02 <alzidni2000@gmail.com>
2019-10-03 14:32:46 +0800

    push zidni_branch

---
+++ application/views/templates/form/filter_login.php
@@ -0,0 +1,39 @@
+    <form action="">
+        <div class="row mb-2">
+            <div class="col-4">
+                <?php
+                $form = array(
+                    'name' => 'month',
+                    'id' => 'month',
+                    'class' => 'form-control',
+                );
+                $attr = $form_data['month'];
+                $form['options'] = $attr['options'];
+                $form['selected'] = $attr['selected'];
+                echo '<label for="" class="control-label">' . $attr["label"] . '</label>';
+                echo form_dropdown($form);
+                ?>
+            </div>
+            <div class="col-4">
+                <?php
+                $form = array(
+                    'name' => 'opd',
+                    'id' => 'opd',
+                    'class' => 'form-control',
+                );
+                $attr = $form_data['opd'];
+                $form['options'] = $attr['options'];
+                $form['selected'] = $attr['selected'];
+                echo '<label for="" class="control-label">' . $attr["label"] . '</label>';
+                echo form_dropdown($form);
+                ?>
+                <input type="hidden" name="group_by" value="date">
+            </div>
+            <div class="col-4" style="margin-top:32px">
+                <button type="submit" class="btn btn-primary btn-sm" data-toggle="modal" style="margin-left: 5px;">
+                    Filter
+                </button>
+            </div>
+        </div>
+    </form>
+    <!-- - -->
